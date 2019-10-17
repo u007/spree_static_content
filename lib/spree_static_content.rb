69a86eed1412b415643b7ebb6dea50dfd5cb9d20
Damian Legawiec <damian@sparksolutions.co>
2019-10-18 00:39:02 +0200

    We don't use either coffee script or sass in this extension
    fixes errors such as https://spree-commerce.slack.com/archives/C0JCDUK0D/p1571343660041500

--- lib/spree_static_content.rb
+++ lib/spree_static_content.rb
@@ -1,8 +1,6 @@
 require 'spree_core'
 require 'spree_static_content/engine'
 require 'spree_static_content/version'
-require 'coffee_script'
-require 'sass/rails'
 require 'spree_extension'
 
 module StaticPage
