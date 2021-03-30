0ba04efd6332ed173092de39337c7989632989b8
KacperMekarski <kacper.mekarski@sparksolutions.co>
2021-03-30 16:42:00 +0200

    fix tests

--- spec/features/static_content_spec.rb
+++ spec/features/static_content_spec.rb
@@ -62,14 +62,14 @@ RSpec.feature 'Static Content Page', :js do
     end
 
     scenario 'has valid title' do
-      create(:page, title: 'Title', meta_title: 'Meta Title', stores: [store])
+      create(:page, slug: '/page', title: 'Title', meta_title: 'Meta Title', stores: [store])
       visit '/page'
-      expect(page).to have(:title, 'Title')
+      expect(page).to have_title('Title')
     end
 
     context 'when meta title is present' do
       scenario 'has valid meta title' do
-        create(:page, title: 'Title', meta_title: 'Meta Title', stores: [store])
+        create(:page, slug: '/page', title: 'Title', meta_title: 'Meta Title', stores: [store])
         visit '/page'
         expect(page).to have_meta(:title, 'Meta Title')
       end
@@ -77,7 +77,7 @@ RSpec.feature 'Static Content Page', :js do
 
     context 'when meta title is not present' do
       scenario 'has meta title like title' do
-        create(:page, title: 'Title', meta_title: nil, stores: [store])
+        create(:page, slug: '/page', title: 'Title', meta_title: nil, stores: [store])
         visit '/page'
         expect(page).to have_meta(:title, 'Title')
       end
